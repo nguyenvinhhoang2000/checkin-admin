3ee4d2f55f2383cbeb2769df25bf169eb67ddce6
HOANGNBD <Duchoang020120@gmail.com>
2024-01-16 09:34:42 +0700

    m

--- src/components/layouts/header/index.jsx
+++ src/components/layouts/header/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function Header() {
   return (
     <div className="flex flex-row justify-between p-6 sm:flex sm:flex-row sm:items-center sm:justify-end">
-      {" "}
       <div>Vinh Thai</div>
     </div>
   );

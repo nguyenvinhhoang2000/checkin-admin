31a2122bd77e81b07eb16730f0d0627da212210b
HOANGNBD <Duchoang020120@gmail.com>
2024-01-16 09:28:50 +0700

    header-layout-footer

--- src/components/layouts/footer/index.jsx
+++ src/components/layouts/footer/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return <div className="p-6">Footer</div>;
 }
 
 export default Footer;
--- src/components/layouts/layout/index.jsx
+++ src/components/layouts/layout/index.jsx
@@ -1,47 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Layout } from "antd";
-import { useBoolean } from "usehooks-ts";
 
 import Footer from "../footer";
 import Header from "../header";
 
-import ModalSideBar from "./draw-sidebar";
-import SideBarHeader from "./header-sidebar";
-import MenuSideBar from "./menu";
-
 function AppLayout() {
-  const { value: isCollapsed, toggle: onToggleCollapsed } = useBoolean();
-
-  const {
-    value: isOpenDrawSidebar,
-    setTrue: onOpenDrawSideBar,
-    setFalse: onCloseDrawSideBar,
-  } = useBoolean();
-
   return (
     <Layout className="min-h-screen">
       <Layout.Sider
         trigger={null}
-        className="hidden bg-sideBar sm:block"
+        className="hidden bg-sideBar text-white sm:block"
         width={220}
-        collapsed={isCollapsed}
       >
-        <div className="hidden bg-sideBar sm:block sm:h-full">
-          <SideBarHeader
-            isCollapsed={isCollapsed}
-            onToggleCollapsed={onToggleCollapsed}
-          />
-          <MenuSideBar inlineCollapsed={isCollapsed} />
-        </div>
-        <ModalSideBar
-          isOpenDrawSidebar={isOpenDrawSidebar}
-          onCloseDrawSideBar={onCloseDrawSideBar}
-        />
+        Side Bar
       </Layout.Sider>
       <Layout>
         <div className="bg-white">
-          <Header onOpenDrawSideBar={onOpenDrawSideBar} />
+          <Header />
         </div>
         <Layout.Content className="px-6">
           <Outlet />

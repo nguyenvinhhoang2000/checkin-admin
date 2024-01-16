78cbaed6f3a0ee90368ea0b72fb286d196b465be
HOANGNBD <Duchoang020120@gmail.com>
2024-01-16 10:53:29 +0700

    m

--- src/components/layouts/header/index.jsx
+++ src/components/layouts/header/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import AppIcon from "@/components/apps/AppIcon";
+
 function Header() {
   return (
     <header className="flex flex-row justify-between bg-white sm:flex sm:flex-row sm:items-center sm:justify-end">
       <div className="p-6">Vinh Thai</div>
+      <AppIcon
+        className="text-red-600 hover:text-blue-500"
+        src="/icons/id-card-icon.svg#id"
+      />
     </header>
   );
 }

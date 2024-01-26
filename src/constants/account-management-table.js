6558814dfdb64cb3e779a203c7a17be476536636
HOANGNBD <Duchoang020120@gmail.com>
2024-01-26 15:46:24 +0700

    edit: columns of accout management

---
+++ src/constants/account-management-table.js
@@ -0,0 +1,24 @@
+const ACCOUNT_MANAGEMENT_COLUMNS = {
+  NAME: {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  EMAIL: {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  BRANCH: {
+    title: "Branch",
+    dataIndex: "branch",
+    key: "branch",
+  },
+  ACTIONS: {
+    title: "Actions",
+    dataIndex: "actions",
+    key: "actions",
+  },
+};
+
+export default ACCOUNT_MANAGEMENT_COLUMNS;
--- src/routes/account-management/components/table/index.jsx
+++ src/routes/account-management/components/table/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Button, Table } from "antd";
+import { Button, message, Table } from "antd";
 
 import AppIcon from "@/components/apps/app-icon";
+
+import ACCOUNT_MANAGEMENT_COLUMNS from "@/constants/account-management-table";
 
 function TablerAccountManagement() {
   const dataSource = [
@@ -23,29 +25,44 @@ function TablerAccountManagement() {
 
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      ...ACCOUNT_MANAGEMENT_COLUMNS.NAME,
       width: "29.3%",
     },
     {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
+      ...ACCOUNT_MANAGEMENT_COLUMNS.EMAIL,
+      width: "29.3%",
+      render: (text) => {
+        const onClickClipboard = () => {
+          navigator.clipboard.writeText(text);
+          message.success("Coppy to clipboard");
+        };
+        return (
+          <div>
+            <span>{text} </span>
+            <Button
+              type="link"
+              className="!h-fit !w-fit p-0"
+              icon={
+                <AppIcon
+                  width={12}
+                  height={12}
+                  className="p-0 text-primary-1"
+                  src="/icons/coppy-icon.svg#id"
+                />
+              }
+              onClick={onClickClipboard}
+            />
+          </div>
+        );
+      },
+    },
+    {
+      ...ACCOUNT_MANAGEMENT_COLUMNS.BRANCH,
       width: "29.3%",
     },
     {
-      title: "Branch",
-      dataIndex: "branch",
-      key: "branch",
-      width: "29.3%",
-    },
-    {
-      title: "Action",
-      dataIndex: "action",
-      key: "action",
+      ...ACCOUNT_MANAGEMENT_COLUMNS.ACTIONS,
       width: "12%",
-
       render: () => {
         return (
           <div className="flex flex-row gap-[1.25rem]">
